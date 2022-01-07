a2f6240f7c0f349299cd6b0a838b28beaab61782
Joaquin Philip Tandang <joaquinphilip.tandang@evsu.edu.ph>
2022-01-07 08:00:49 +0800

    Committed to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Calculator } from './calculator.component/calculator.component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Calculator.ComponentComponent
   ],
   imports: [
     BrowserModule,
