8c34c72261e13e6c528415bd06e07611f04ade4b
Joaquin Philip Tandang <joaquinphilip.tandang@evsu.edu.ph>
2022-01-07 09:05:21 +0800

    ERRORS EVERYWEHERE

--- src/app/calculator.component/calculator.component.component.ts
+++ src/app/calculator.component/calculator.component.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+ 
 @Component({
-  selector: 'app-calculator.component',
-  templateUrl: './calculator.component.component.html',
-  styleUrls: ['./calculator.component.component.css']
+  selector: 'ng-calculator',
+  templateUrl: './calculator.component.html',
 })
-export class Calculator.ComponentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CalculatorComponent {
+ 
 }
+
