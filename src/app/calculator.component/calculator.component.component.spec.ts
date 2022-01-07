9a7fabb79ee3a02fb7cafaa9c3bc3cfce92bace8
Joaquin Philip Tandang <joaquinphilip.tandang@evsu.edu.ph>
2022-01-07 09:00:23 +0800

    modifications galore im dying

--- src/app/calculator.component/calculator.component.component.spec.ts
+++ src/app/calculator.component/calculator.component.component.spec.ts
@@ -8,29 +8,3 @@ import { Component} from '@angular/core';
 export class CalculatorComponent {
  
 }
-
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { Calculator.ComponentComponent } from './calculator.component.component';
-
-describe('Calculator.ComponentComponent', () => {
-  let component: Calculator.ComponentComponent;
-  let fixture: ComponentFixture<Calculator.ComponentComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ Calculator.ComponentComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(Calculator.ComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
